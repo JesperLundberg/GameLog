5a1359988ef16f14547f8044cc288c09d6485db4
Jesper Lundberg <jesper.lundberg@sogeti.com>
2020-06-23 16:21:55 +0200

    Remove unused view

--- src/GameLog.Web/Controllers/HomeController.cs
+++ src/GameLog.Web/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using GameLog.Web.Models;
@@ -11,19 +7,14 @@ namespace GameLog.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        public ILogger<HomeController> Logger { get; set; }
 
         public HomeController(ILogger<HomeController> logger)
         {
-            _logger = logger;
+            Logger = logger;
         }
-
+        
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
--- src/GameLog.Web/Models/ErrorViewModel.cs
+++ src/GameLog.Web/Models/ErrorViewModel.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace GameLog.Web.Models
 {
     public class ErrorViewModel
